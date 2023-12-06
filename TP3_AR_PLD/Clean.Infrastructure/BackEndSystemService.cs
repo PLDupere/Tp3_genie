1bbd9d0132e4ddf31fca936619acb5f7c90ae4d0
pierl <pierl@PPUD.local>
2023-12-06 14:25:24 -0500

    BackendSystemService ... Request ??

---
+++ TP3_AR_PLD/Clean.Infrastructure/BackEndSystemService.cs
@@ -0,0 +1,24 @@
+﻿using Clean.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Clean.Infrastructure
+{
+    public class BackEndSystemService : IBackEndSystemService
+    {
+        public async Task SendRequestToBackEnd(Request request, string directory)
+        {
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add("ns0", "http://EAISolution.Request");
+
+            string xml = XmlTranfromHelper.Serialize(request, namespaces);
+
+            await File.WriteAllTextAsync(directory + "Request" + DateTime.Now.Ticks + ".xml", xml);
+
+        }
+    }
+}
---
+++ TP3_AR_PLD/Clean.Infrastructure/XmlTranfromHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
+
+namespace Clean.Infrastructure
+{
+    class XmlTranfromHelper
+    {
+        public static string Serialize<T>(T toSerialize, XmlSerializerNamespaces namespaces)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(toSerialize.GetType());
+
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            settings.OmitXmlDeclaration = true;
+
+            using (var textWriter = new StringWriter())
+            {
+                using (XmlWriter xmlWriter = XmlWriter.Create(textWriter, settings))
+                {
+                    xmlSerializer.Serialize(xmlWriter, toSerialize, namespaces);
+                    return textWriter.ToString();
+                }
+            }
+        }
+    }
+}
